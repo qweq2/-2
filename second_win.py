1b169d8c92453d27860de0784cf5b8da025d3e61
qweq2 <134150891+qweq2@users.noreply.github.com>
2023-06-03 18:04:05 +0300

    second_win.py

--- second_win.py
+++ second_win.py
@@ -1,34 +1,35 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QLable, QApplication,QPushButton,QVBoxLayout,QHBoxLayout,QLineEdit
+from PyQt5.QtCore import Qt , QTime , QTimer
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QListWidget, QLineEdit
 from instr import *
-class TestWin(QtWidget):
+from final_win import *
+class TestWin(QWidget):
     def __init__(self):
-        super()__init__()
+        super().__init__()
         self.set_appear()
         self.initUI()
         self.connects()
         self.show()
     def set_appear(self):
-        self.setWindowTitle(set_text)
+        self.setWindowTitle(txt_title)
         self.resize(win_width,win_height)
         self.move(win_x,win_y)
     def initUI(self):
-        self.txt_title = QLable(txt_title)
-        self.txt_name = QLable(txt_name)
-        self.txt_hintname = QLineEdit(hintname)
-        self.txt_age = QLable(txt_age)
-        self.txt_hintage = QLineEdit(hintage)
-        self.txt_test1 = QLable(txt_test1)
-        self.txt_starttest1 = QPushButton(txt_starttest1)
-        self.txt_hinttest1 = QLineEdit(hinttest1)
-        self.txt_test2 = QLable(txt_test2)
-        self.txt_starttest2 = QPushButton(txt_starttest2)
-        self.txt_test3 = QLable(txt_test3)
-        self.txt_starttest3 = QPushButton(txt_starttest3)
-        self.txt_hinttest2 = QLineEdit(hinttest2)
-        self.txt_hinttest3 = QLineEdit(hinttest3)
-        self.txt_sendresults = QPushButton(txt_sendresults)
-        self.txt_timer = QLable(txt_timer)
+        self.txt_title = QLabel(txt_title)
+        self.txt_name = QLabel(txt_name)
+        self.txt_hintname = QLineEdit(txt_hintname)
+        self.txt_age = QLabel(txt_age)
+        self.txt_hintage = QLineEdit(txt_hintage)
+        self.txt_test1 = QLabel(txt_test1)
+        self.btn_starttest1 = QPushButton(txt_starttest1)
+        self.txt_hinttest1 = QLineEdit(txt_hinttest1)
+        self.txt_test2 = QLabel(txt_test2)
+        self.btn_starttest2 = QPushButton(txt_starttest2)
+        self.txt_test3 = QLabel(txt_test3)
+        self.btn_starttest3 = QPushButton(txt_starttest3)
+        self.txt_hinttest2 = QLineEdit(txt_hinttest2)
+        self.txt_hinttest3 = QLineEdit(txt_hinttest3)
+        self.btn_sendresults = QPushButton(txt_sendresults)
+        self.txt_timer = QLabel(txt_timer)
         self.h_line = QHBoxLayout()
         self.r_line = QVBoxLayout()
         self.l_line = QVBoxLayout()
@@ -44,11 +45,23 @@ class TestWin(QtWidget):
         self.l_line.addWidget(self.txt_test3)
         self.l_line.addWidget(self.txt_starttest3)
         self.l_line.addWidget(self.txt_hinttest2)
-        self.l_line.addwidget(self.txt_hinttest3)
-        self.h_line.addWidget(self.txt_sendresults)
-        self.r_line.addWidget(self.txt_timer)
+        self.l_line.addWidget(self.txt_hinttest3)
+        self.l_line.addWidget(self.txt_sendresults,alignment = Qt.AlignCenter)
+        self.h_line.addWidget(self.txt_timer)
+        self.h_line.addLayout(self.l_line)
+        self.h_line.addLayout(self.r_line)
+        self.setLayout(self.h_line)
+        
+    def timer_test(self):
+        time = QTime(0,1,0)
+        self.timer.timeout.connect(self.timerEvent)
+        self.timer.start(1000)
+    def timerEvent(self):
+
+
     def connects(self):
-        self.bnt_next.clicked.connect(self.next_click)
+        self.btn_sendresults.clicked.connect(self.next_click)
+        self.btn_starttest1.clicked.connect(self.timer_test)
     def next_click(self):
         self.hide()
         self.fw = FinWin()
