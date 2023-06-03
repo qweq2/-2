1a90891d5641515315d9307faedd6932fb89bc9a
qweq2 <134150891+qweq2@users.noreply.github.com>
2023-06-03 18:03:35 +0300

    my_app.py

--- my_app.py
+++ my_app.py
@@ -1,27 +1,38 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QLable, QApplication,QPushButton,QVBoxLayout,QHBoxLayout,QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QListWidget, QLineEdit
+ 
 from instr import *
+from second_win import *
+
 class MainWin(QWidget):
     def __init__(self):
-        super().__init__
-        self.set_appear()
-        self.initUI()
-        self.connects()
-        self.show()
-    def set_appear(self):
-        self.setWindowTitle(txt_title)
-        self.resize(win_width,win_height)
-        self.move(win_x,win_y)
-    def initUI():
-        self.hello_text = QLable(txt_hello)
-        self.instruction = QLable(txt_instruction)
-        self.button = QPushButton(txt_next)
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.hello_text)
-        self.layout.addWidget(self.instruction)
-        self.layout.addWidget(self.button)
+       super().__init__()
+       self.initUI()
+       self.connects()
+       self.set_appear()
+       self.show()
+              
+    def initUI(self):
+       self.btn_next = QPushButton(txt_next, self)
+       self.hello_text = QLabel(txt_hello)
+       self.instruction = QLabel(txt_instruction)
+ 
+       self.layout_line = QVBoxLayout()
+       self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+       self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+       self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+       self.setLayout(self.layout_line)
+   
     def connects(self):
-        self.btn_next.clicked.connect(self.next_click)
+       self.btn_next.clicked.connect(self.next_click)
     def next_click(self):
-        self.hide
-        self.tw = TestWin()
+       self.hide()
+       self.tw = TestWin()
+    def set_appear(self):
+       self.setWindowTitle(txt_title)
+       self.resize(win_width, win_height)
+       self.move(win_x, win_y)
+ 
+app = QApplication([])
+mw = MainWin()
+app.exec_()
